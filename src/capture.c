norihiro/audio-network-analyzer

commit 18063a87fbef6b9338bfed2281844a2cf2919a8b

Fix glitch when changing frequency

src/capture.c
@@ -15,7 +15,6 @@ struct capture_s
 	audio_context_t *ctx;
 	pa_stream *stream;
 
-	double freq;
 	int sum_n;
 	double sum_real, sum_imag;
 	int i_sample;
@@ -39,7 +38,6 @@ capture_t *capture_create(audio_context_t *ctx)
 		fprintf(stderr, "Error: pa_stream_new failed\n");
 		goto err;
 	}
-	c->freq = 0.0;
 
 	ret = pa_stream_connect_record(
 			c->stream,
@@ -90,18 +88,24 @@ static void capture_callback(pa_stream *p, size_t nbytes_, void *userdata)
 	for (int i=0; i<n_sample; i++) {
 		if (c->ctx->data[c->i_data].freq <= 0)
 			break;
-		if (c->n_read < c->ctx->data[c->i_data].ts_begin_cap)
-			capture_start(c, c->ctx->data[c->i_data].freq);
+		if (c->n_read == c->ctx->data[c->i_data].ts_begin_cap) {
+			c->sum_n = 0;
+			c->sum_real = 0.0;
+			c->sum_imag = 0.0;
+			c->i_sample = 0;
+		}
 
-		const int rate = c->ctx->spec.rate;
-		double v_real = sin(c->i_sample * c->freq * 2.0 * M_PI / rate);
-		double v_imag = cos(c->i_sample * c->freq * 2.0 * M_PI / rate);
-		double v = (double)buf[i*2+0] + (double)buf[i*2+1];
-		c->sum_n += 1;
-		c->sum_real += v * v_real / 32767.;
-		c->sum_imag += v * v_imag / 32767.;
-		if (++c->i_sample >= rate)
-			c->i_sample -= rate;
+		if (c->ctx->data[c->i_data].ts_begin_cap <= c->n_read) {
+			double freq = c->ctx->data[c->i_data].freq;
+			const int rate = c->ctx->spec.rate;
+			double v_real = sin(c->i_sample * freq * 2.0 * M_PI / rate);
+			double v_imag = cos(c->i_sample * freq * 2.0 * M_PI / rate);
+			double v = (double)buf[i*2+0] + (double)buf[i*2+1];
+			c->sum_n += 1;
+			c->sum_real += v * v_real / 32767.;
+			c->sum_imag += v * v_imag / 32767.;
+			++ c->i_sample;
+		}
 
 		c->n_read += 1;
 		if (c->n_read >= c->ctx->data[c->i_data].ts_end) {
@@ -114,16 +118,6 @@ static void capture_callback(pa_stream *p, size_t nbytes_, void *userdata)
 	}
 
 	pa_stream_drop(p);
-}
-
-int capture_start(capture_t *c, double frequency)
-{
-	c->freq = frequency;
-	c->sum_n = 0;
-	c->sum_real = 0.0;
-	c->sum_imag = 0.0;
-
-	return 0;
 }
 
 int capture_measure(capture_t *c, double *real, double *imag)

src/capture.h
@@ -8,6 +8,5 @@ typedef struct capture_s capture_t;
 capture_t *capture_create(audio_context_t *);
 void capture_release(capture_t *);
 
-int capture_start(capture_t *, double frequency);
 int capture_measure(capture_t *c, double *real, double *imag);
 uint64_t capture_ts(capture_t *);

src/main.c
@@ -249,6 +249,8 @@ int main(int argc, char **argv)
 		if (p - guardband_sec < min_cycle / data[i].freq)
 			p = min_cycle / data[i].freq + guardband_sec;
 
+		p = ceil(p * data[i].freq) / data[i].freq;
+
 		const uint64_t period = 48000 * p;
 		data[i].ts_begin_src = ts_end;
 		data[i].ts_begin_cap = ts_end + 48000 * guardband_sec;

src/source.c
@@ -15,7 +15,6 @@ struct source_s
 	audio_context_t *ctx;
 	pa_stream *stream;
 
-	double freq;
 	int i_sample;
 
 	uint64_t n_written;
@@ -36,7 +35,6 @@ source_t *source_create(audio_context_t *ctx)
 		fprintf(stderr, "Error: pa_stream_new failed\n");
 		goto err;
 	}
-	s->freq = 0.0;
 
 	ret = pa_stream_connect_playback(
 			s->stream,
@@ -91,21 +89,20 @@ static void source_callback(pa_stream *p, size_t nbytes, void *userdata)
 	int n_sample = nbytes / 4;
 	if (n_sample > N_SAMPLES) n_sample = N_SAMPLES;
 	for (int i=0; i<n_sample; i++) {
-		if (s->n_written >= s->ctx->data[s->i_data].ts_begin_src) {
-			if (s->ctx->data[s->i_data].freq > 0)
-				s->freq = s->ctx->data[s->i_data].freq;
-			else
-				s->freq = 0;
+		if (s->n_written == s->ctx->data[s->i_data].ts_begin_src) {
+			s->i_sample = 0;
 		}
 
+		double freq = s->ctx->data[s->i_data].freq;
+		if (freq < 0.0)
+			freq = 0.0;
 		const int rate = s->ctx->spec.rate;
-		double v = sin(s->i_sample * s->freq * 2.0 * M_PI / rate);
-		const double eps = sin(1 * s->freq * 2.0 * M_PI / rate);
+		double v = sin(s->i_sample * freq * 2.0 * M_PI / rate);
+		const double eps = sin(1 * freq * 2.0 * M_PI / rate);
 
 		buf[i*2+0] = (int16_t)(32767*v);
 		buf[i*2+1] = (int16_t)(32767*v);
-		if (++s->i_sample >= rate)
-			s->i_sample -= rate;
+		++ s->i_sample;
 
 		s->n_written += 1;
 		if (s->n_written >= s->ctx->data[s->i_data].ts_end) {

commit ee56e63f5dd9fad55688a4d8a85887c2a3043a58

Fix phase calculation
The previous commit 18063a8 introduced a bug that the capture has
different offset to calculate the phase.

src/capture.c
@@ -88,11 +88,11 @@ static void capture_callback(pa_stream *p, size_t nbytes_, void *userdata)
 	for (int i=0; i<n_sample; i++) {
 		if (c->ctx->data[c->i_data].freq <= 0)
 			break;
-		if (c->n_read == c->ctx->data[c->i_data].ts_begin_cap) {
+		if (c->n_read == c->ctx->data[c->i_data].ts_begin_src) {
+			c->i_sample = 0;
 			c->sum_n = 0;
 			c->sum_real = 0.0;
 			c->sum_imag = 0.0;
-			c->i_sample = 0;
 		}
 
 		if (c->ctx->data[c->i_data].ts_begin_cap <= c->n_read) {
@@ -104,11 +104,12 @@ static void capture_callback(pa_stream *p, size_t nbytes_, void *userdata)
 			c->sum_n += 1;
 			c->sum_real += v * v_real / 32767.;
 			c->sum_imag += v * v_imag / 32767.;
-			++ c->i_sample;
 		}
 
+		++ c->i_sample;
+
 		c->n_read += 1;
-		if (c->n_read >= c->ctx->data[c->i_data].ts_end) {
+		if (c->n_read == c->ctx->data[c->i_data].ts_end) {
 			struct data_entry_t *data = c->ctx->data + c->i_data;
 			capture_measure(c, &data->real, &data->imag);
 			if (c->ctx->print_entry)