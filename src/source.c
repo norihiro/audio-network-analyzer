415f9993f088f666a8f0db19c9906ff72db9abc8
Norihiro Kamae <norihiro@nagater.net>
2021-12-23 10:06:16 +0900

    Fix audio output on RHEL8
    Prior to this commit, the source stream was filled at most 1024 samples,
    where write callback sometimes requests more data specified by `nbytes`.
    If `nbytes` data is not filled, write callback was not called so that
    the audio was not output.

--- src/source.c
+++ src/source.c
@@ -84,31 +84,34 @@ static void source_callback(pa_stream *p, size_t nbytes, void *userdata)
 {
 	source_t *s = userdata;
 
+	while (nbytes > 0) {
 #define N_SAMPLES 1024
-	int16_t buf[N_SAMPLES*2];
-	int n_sample = nbytes / 4;
-	if (n_sample > N_SAMPLES) n_sample = N_SAMPLES;
-	for (int i=0; i<n_sample; i++) {
-		if (s->n_written == s->ctx->data[s->i_data].ts_begin_src) {
-			s->i_sample = 0;
-		}
+		int16_t buf[N_SAMPLES*2];
+		int n_sample = nbytes / 4;
+		if (n_sample > N_SAMPLES) n_sample = N_SAMPLES;
+		for (int i=0; i<n_sample; i++) {
+			if (s->n_written == s->ctx->data[s->i_data].ts_begin_src) {
+				s->i_sample = 0;
+			}
 
-		double freq = s->ctx->data[s->i_data].freq;
-		if (freq < 0.0)
-			freq = 0.0;
-		const int rate = s->ctx->spec.rate;
-		double v = sin(s->i_sample * freq * 2.0 * M_PI / rate);
+			double freq = s->ctx->data[s->i_data].freq;
+			if (freq < 0.0)
+				freq = 0.0;
+			const int rate = s->ctx->spec.rate;
+			double v = sin(s->i_sample * freq * 2.0 * M_PI / rate);
 
-		buf[i*2+0] = (int16_t)(32767*v);
-		buf[i*2+1] = (int16_t)(32767*v);
-		++ s->i_sample;
+			buf[i*2+0] = (int16_t)(32767*v);
+			buf[i*2+1] = (int16_t)(32767*v);
+			++ s->i_sample;
 
-		s->n_written += 1;
-		if (s->n_written >= s->ctx->data[s->i_data].ts_end) {
-			if (s->ctx->data[s->i_data].freq > 0.0) {
-				++ s->i_data;
+			s->n_written += 1;
+			if (s->n_written >= s->ctx->data[s->i_data].ts_end) {
+				if (s->ctx->data[s->i_data].freq > 0.0) {
+					++ s->i_data;
+				}
 			}
 		}
+		pa_stream_write(p, buf, n_sample*4, NULL, 0, PA_SEEK_RELATIVE);
+		nbytes -= n_sample*4;
 	}
-	pa_stream_write(p, buf, n_sample*4, NULL, 0, PA_SEEK_RELATIVE);
 }
